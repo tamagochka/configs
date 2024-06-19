fbca7ec879228d50a2bed3472737a93c0f319a9c
tamagochka <36785867+tamagochka@users.noreply.github.com>
2024-06-19 21:56:18 +0300

    added tmux config and vim theme to init_script_ubuntu.sh

--- init_script_ubuntu.sh
+++ init_script_ubuntu.sh
@@ -10,12 +10,17 @@ apt install -y zsh vim wget screenfetch git tmux mc curl
 echo "-------------------- downloading configuration files --------------------"
 wget -O .zshrc https://raw.githubusercontent.com/tamagochka/configs/main/.zshrc
 wget -O .vimrc https://raw.githubusercontent.com/tamagochka/configs/main/.vimrc
+wget -O orbital.vim https://raw.githubusercontent.com/tamagochka/configs/main/orbital.vim
+wget -O tmux.conf https://raw.githubusercontent.com/tamagochka/configs/main/tmux.conf
 
 echo "-------------------- coping files and setting permissions --------------------"
 cp .zshrc /root
 cp .vimrc /root
 chown user:user .zshrc
 chown user:user .vimrc
+mkdir -p /etc/vim/colors
+mv orbital.vim /etc/vim/colors
+mv tmux.conf /etc
 
 echo "-------------------- setting default shell --------------------"
 chsh -s /bin/zsh user
