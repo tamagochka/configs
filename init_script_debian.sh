b92a76d66fe8dc64e146eddfd14afcc3f1abf237
tamagochka <36785867+tamagochka@users.noreply.github.com>
2024-06-19 21:55:52 +0300

    added tmux.conf and vim theme to init_script_debian.sh

--- init_script_debian.sh
+++ init_script_debian.sh
@@ -12,14 +12,16 @@ wget -O .zshrc https://raw.githubusercontent.com/tamagochka/configs/main/.zshrc
 wget -O .vimrc https://raw.githubusercontent.com/tamagochka/configs/main/.vimrc
 wget -O id_ed25519.pub https://raw.githubusercontent.com/tamagochka/configs/main/id_ed25519.pub
 wget -O orbital.vim https://raw.githubusercontent.com/tamagochka/configs/main/orbital.vim
-
+wget -O tmux.conf https://raw.githubusercontent.com/tamagochka/configs/main/tmux.conf
 
 echo "-------------------- coping files and setting permissions --------------------"
 cp .zshrc /root
 cp .vimrc /root
 chown user:user .zshrc
 chown user:user .vimrc
-#cp orbital.vim /usr
+mkdir -p /etc/vim/colors
+mv orbital.vim /etc/vim/colors
+mv tmux.conf /etc
 
 echo "-------------------- setting default shell --------------------"
 chsh -s /bin/zsh user
@@ -32,7 +34,7 @@ cat id_ed25519.pub > .ssh/authorized_keys
 chmod 700 .ssh
 chmod 600 .ssh/authorized_keys
 chown -R user:user .ssh
-rm id_rsa.pub
+rm id_ed25519.pub
 
 echo "-------------------- setting timezone --------------------"
 timedatectl set-timezone 'Europe/Moscow'
